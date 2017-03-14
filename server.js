e1dd128032ffcc6568cb5be2bb91458b16283c35
Jaime Irazabal <jaimeirazabal1@gmail.com>
2017-03-14 09:41:14 -0430

    agregando integrantes, y que los demas lean cuando escribe el otro y que cuando se conecta y desconecta alguien

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+var users = [];
 
 app.use('/static',express.static(__dirname+'/static'));
 app.set('views',__dirname+'/views');
@@ -21,11 +21,69 @@ http.listen(3000,function(){
 
 //sockets
 io.on('connection',function(socket){
-	console.log("se conecto: ",socket.id)
+	//console.log("se conecto: ",socket.id)
+	io.sockets.connected[socket.id].emit("newid",socket.id);
+
 	socket.on('user image',function(image){
 		io.sockets.emit('addimage',image.user,image.file);
+	});
+	socket.on('new user',function(user){
+		console.log("user id:",user)
+		users.push({
+			id:user.id,
+			nickname:user.newuser
+		});
+		io.sockets.emit("refreshusers",users);
+		console.log(users);
 	});
 	socket.on('new message',function(newMessage){
 		io.sockets.emit('newmessage',newMessage.user,newMessage.message);
 	});
+	socket.on("writing",function(id){
+          	for (i = 0; i < users.length; i++) { 
+          		if (users[i]) {
+
+		          	if (users[i].id == id.id) {
+		          		for (var j = 0; j <users.length ; j++) {
+
+		          			if (users[j] && users[j].id != id.id) {
+
+		          				io.sockets.connected[users[j].id].emit("write","Esta Escribiendo "+users[i].nickname);
+		          			}
+		          		}
+		          		
+		          	}
+          		}
+			}		
+	});
+	socket.on("writingstop",function(id){
+          	for (i = 0; i < users.length; i++) { 
+          		if (users[i]) {
+
+		          	if (users[i].id == id.id) {
+		          		io.sockets.emit("writestop","");
+		          		
+		          	}
+          		}
+			}		
+	});
+	socket.on ( "disconnect" , function ()
+       {
+          
+          console.log(users);
+          	for (i = 0; i < users.length; i++) { 
+          		if (users[i]) {
+
+		          	if (users[i].id == socket.id) {
+		          		io.sockets.emit("disconnect",users[i].nickname);
+		          		delete users[i]
+		          		console.log("Se borró una vaina!");
+		          	}
+          		}
+			}
+          console.log(users);
+          
+          io.sockets.emit("refreshusers",users);
+
+       })
 });
